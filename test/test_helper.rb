c10ace300c5e2c1fc0753c0f191f6e56f371808e
Ronaldo M. Ferraz <ronaldoferraz@gmail.com>
2011-01-08 22:25:31 -0800

    Added a function creator helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,3 +5,19 @@ require "test/unit"
 require "llvm/core"
 require "llvm/execution_engine"
 require "llvm/transforms/scalar"
+
+def define_function(argument_types, argument_values, return_type)
+
+  test_module = LLVM::Module.create("test_module")
+
+  test_module.functions.add("test_function", argument_types, return_type) do |function, *arguments|
+    yield(LLVM::Builder.create, function, *arguments)
+  end
+
+  test_module.verify
+
+  engine = LLVM::ExecutionEngine.create_jit_compiler(test_module)
+
+  engine.run_function(test_module.functions["test_function"], *argument_values)
+
+end
