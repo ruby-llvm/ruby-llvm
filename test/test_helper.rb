a88bf1996fe31ac857c3e714a54a5fa84b82a159
Ronaldo M. Ferraz <ronaldoferraz@gmail.com>
2011-01-11 19:27:35 -0200

    Made the helper functions more flexible

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,18 +6,41 @@ require "llvm/core"
 require "llvm/execution_engine"
 require "llvm/transforms/scalar"
 
-def run_function(argument_types, argument_values, return_type)
+def define_module(module_name)
 
-  test_module = LLVM::Module.create("test_module")
+  new_module = LLVM::Module.create(module_name)
 
-  test_module.functions.add("test_function", argument_types, return_type) do |function, *arguments|
+  yield new_module
+
+  new_module.verify
+
+  new_module
+
+end
+
+def define_function(host_module, function_name, argument_types, return_type)
+
+  host_module.functions.add(function_name, argument_types, return_type) do |function, *arguments|
     yield(LLVM::Builder.create, function, *arguments)
   end
 
-  test_module.verify
+end
 
-  engine = LLVM::ExecutionEngine.create_jit_compiler(test_module)
+def run_function_on_module(host_module, function_name, *argument_values)
 
-  engine.run_function(test_module.functions["test_function"], *argument_values)
+  LLVM::ExecutionEngine.
+    create_jit_compiler(host_module).
+    run_function(host_module.functions[function_name], *argument_values)
 
 end
+
+def run_function(argument_types, argument_values, return_type, &block)
+
+  test_module = define_module("test_module") do |host_module|
+    define_function(host_module, "test_function", argument_types, return_type, &block)
+  end
+
+  run_function_on_module(test_module, "test_function", *argument_values)
+
+end
+
