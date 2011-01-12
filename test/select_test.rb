8fe3077c2ec6f8c4ce49bbcfaa855daae85b89bf
Ronaldo M. Ferraz <ronaldoferraz@gmail.com>
2011-01-12 18:04:52 -0200

    Added basic select tests

---
+++ test/select_test.rb
@@ -0,0 +1,22 @@
+require "test_helper"
+
+class SelectTestCase < Test::Unit::TestCase
+
+  def setup
+    LLVM.init_x86
+  end
+
+  def test_select
+    assert 0, select_function(0).to_i
+    assert 1, select_function(1).to_i
+  end
+
+  def select_function(value)
+    run_function([LLVM::Int1], [value], LLVM::Int) do |builder, function, *arguments|
+      entry = function.basic_blocks.append
+      builder.position_at_end(entry)
+      builder.ret(builder.select(arguments.first, LLVM::Int(0), LLVM::Int(1)))
+    end
+  end
+
+end
