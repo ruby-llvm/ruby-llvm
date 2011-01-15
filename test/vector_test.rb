8cf0dc59a9cc427fa5a1e70d0e69fe3c5993a2ca
Ronaldo M. Ferraz <ronaldoferraz@gmail.com>
2011-01-15 03:07:47 -0200

    Added a vector shuffle test

--- test/vector_test.rb
+++ test/vector_test.rb
@@ -25,7 +25,11 @@ class VectorTestCase < Test::Unit::TestCase
   end
 
   def test_vector_elements
-    assert_equal 5, run_vector_elements(2, 3).to_i
+    assert_equal 2 + 3, run_vector_elements(2, 3).to_i
+  end
+
+  def test_vector_shuffle
+    assert_equal 1 + 4, run_vector_shuffle(1, 2, 3, 4).to_i
   end
 
   def run_vector_elements(value1, value2)
@@ -38,6 +42,22 @@ class VectorTestCase < Test::Unit::TestCase
       vector = builder.insert_element(vector, arguments.last, LLVM::Int32.from_i(1))
       builder.ret(builder.add(builder.extract_element(vector, LLVM::Int32.from_i(0)),
                               builder.extract_element(vector, LLVM::Int32.from_i(1))))
+    end
+  end
+
+  def run_vector_shuffle(*values)
+    run_function([LLVM::Int, LLVM::Int, LLVM::Int, LLVM::Int], values, LLVM::Int) do |builder, function, *arguments|
+      entry = function.basic_blocks.append
+      builder.position_at_end(entry)
+      vector1 = builder.load(builder.alloca(LLVM::Vector(LLVM::Int, 2)))
+      vector1 = builder.insert_element(vector1, arguments[0], LLVM::Int32.from_i(0))
+      vector1 = builder.insert_element(vector1, arguments[1], LLVM::Int32.from_i(1))
+      vector2 = builder.load(builder.alloca(LLVM::Vector(LLVM::Int, 2)))
+      vector2 = builder.insert_element(vector2, arguments[2], LLVM::Int32.from_i(0))
+      vector2 = builder.insert_element(vector2, arguments[3], LLVM::Int32.from_i(1))
+      vector3 = builder.shuffle_vector(vector1, vector2, LLVM::ConstantVector.const([LLVM::Int(0), LLVM::Int(3)]))
+      builder.ret(builder.add(builder.extract_element(vector3, LLVM::Int32.from_i(0)),
+                              builder.extract_element(vector3, LLVM::Int32.from_i(1))))
     end
   end
 
