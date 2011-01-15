b1ccb896dad9774a5a3c27a22c11ac4bcd5b34f8
Ronaldo M. Ferraz <ronaldoferraz@gmail.com>
2011-01-15 03:03:16 -0200

    Made vector test more explicit

--- test/vector_test.rb
+++ test/vector_test.rb
@@ -25,17 +25,17 @@ class VectorTestCase < Test::Unit::TestCase
   end
 
   def test_vector_elements
-    assert_equal 5, run_vector_elements().to_i
+    assert_equal 5, run_vector_elements(2, 3).to_i
   end
 
-  def run_vector_elements
-    run_function([], [], LLVM::Int) do |builder, function, *arguments|
+  def run_vector_elements(value1, value2)
+    run_function([LLVM::Int, LLVM::Int], [value1, value2], LLVM::Int) do |builder, function, *arguments|
       entry = function.basic_blocks.append
       builder.position_at_end(entry)
       pointer = builder.alloca(LLVM::Vector(LLVM::Int, 2))
       vector = builder.load(pointer)
-      vector = builder.insert_element(vector, LLVM::Int(2), LLVM::Int32.from_i(0))
-      vector = builder.insert_element(vector, LLVM::Int(3), LLVM::Int32.from_i(1))
+      vector = builder.insert_element(vector, arguments.first, LLVM::Int32.from_i(0))
+      vector = builder.insert_element(vector, arguments.last, LLVM::Int32.from_i(1))
       builder.ret(builder.add(builder.extract_element(vector, LLVM::Int32.from_i(0)),
                               builder.extract_element(vector, LLVM::Int32.from_i(1))))
     end
