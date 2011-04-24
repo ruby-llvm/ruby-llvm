2ed0ac576f216d05a3b9e9b72487ebe2bdd05c79
Jeremy Voorhis <jvoorhis@gmail.com>
2011-04-24 10:37:25 -0700

    Added documentation for analysis methods.

--- lib/llvm/analysis.rb
+++ lib/llvm/analysis.rb
@@ -16,10 +16,15 @@ module LLVM
   end
   
   class Module
+    # Verify that the module is valid.
+    # @return [nil, String] human-readable description of any invalid
+    #   constructs if invalid.
     def verify
       do_verification(:return_status)
     end
     
+    # Verify that a module is valid, and abort the process if not.
+    # @return [nil]
     def verify!
       do_verification(:abort_process)
     end
@@ -29,17 +34,21 @@ module LLVM
         str = FFI::MemoryPointer.new(FFI.type_size(:pointer))
         status = C.LLVMVerifyModule(self, action, str)
         case status
-          when 1 then str.read_string
-          else nil
+        when 1 then str.read_string
+        else nil
         end
       end
   end
   
   class Function
+    # Verify that a function is valid.
+    # @return [true, false]
     def verify
       do_verification(:return_status)
     end
 
+    # Verify that a function is valid, and abort the process if not.
+    # @return [true, false]
     def verify!
       do_verification(:abort_process)
     end
