ca999aa6f27308300c3cfbfaaf5c16c7edc76bc6
Ary Borenszweig <ary@esperanto.org.ar>
2011-08-21 18:05:18 -0300

    Added a test that shows that returning twice doesn't work

---
+++ test/return_test.rb
@@ -0,0 +1,12 @@
+require "test_helper"
+
+class ReturnTestCase < Test::Unit::TestCase
+  def test_two_returns
+    value = run_function([], [], LLVM::Int) do |builder, function, *arguments|
+      entry = function.basic_blocks.append
+      builder.position_at_end(entry)
+      builder.ret LLVM::Int(1)
+    end
+    assert_equal 1, value.to_i
+  end
+end
