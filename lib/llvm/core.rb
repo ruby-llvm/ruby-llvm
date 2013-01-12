4d5b4d74e3eb05852ab12c901ffc6f3a5cc8054c
Peter Zotov <whitequark@whitequark.org>
2013-01-12 23:08:35 +0400

    Factor out LLVM#with_message_output.

--- lib/llvm/core.rb
+++ lib/llvm/core.rb
@@ -7,7 +7,25 @@ module LLVM
   module C
     attach_function :dispose_message, :LLVMDisposeMessage, [:pointer], :void
   end
-  
+
+  # Yields a pointer suitable for storing an LLVM output message.
+  # If the block returns +1+ (an error has happened), converts the
+  # result to a string and returns it. Otherwise, returns +nil+.
+  #
+  # @yield  [FFI::MemoryPointer]
+  # @return [String, nil]
+  def self.with_message_output
+    result = nil
+
+    FFI::MemoryPointer.new(FFI.type_size(:pointer)) do |str|
+      status = yield str
+      result = str.read_string if status == 1
+      C.dispose_message str.read_pointer
+    end
+
+    result
+  end
+
   require 'llvm/core/context'
   require 'llvm/core/module'
   require 'llvm/core/type'
