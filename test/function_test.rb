baf5a54612a12864318d8f26a3777af6813c6018
Takanori Ishikawa <takanori.ishikawa@gmail.com>
2013-03-03 19:41:35 +0900

    Fix errors in LLVM::FunctionType#argument_types and LLVM::FunctionType#vararg?

--- test/function_test.rb
+++ test/function_test.rb
@@ -31,3 +31,44 @@ class FunctionTest < Test::Unit::TestCase
   end
 
 end
+
+class FunctionTypeTest < Test::Unit::TestCase
+
+  def test_return_type
+    with_function [], LLVM.Void do |fun|
+      type = fun.function_type
+      assert_equal LLVM.Void, type.return_type
+    end
+
+    with_function [], LLVM::Int32 do |fun|
+      retty = fun.function_type.return_type
+
+      assert_not_nil retty
+      assert_kind_of LLVM::IntType, retty
+      assert_equal 32, retty.width
+    end
+  end
+
+  def test_argument_types
+    with_function [], LLVM.Void do |fun|
+      types = fun.function_type.argument_types
+      assert_equal [], types
+    end
+
+    with_function [LLVM::Int32], LLVM.Void do |fun|
+      types = fun.function_type.argument_types
+      assert_equal 1, types.size
+      a1 = types[0]
+      assert_kind_of LLVM::IntType, a1
+      assert_equal 32, a1.width
+    end
+  end
+
+  def test_vararg
+    with_function [], LLVM.Void do |fun|
+      type = fun.function_type
+      assert !type.vararg?, 'should be false'
+    end
+  end
+
+end
