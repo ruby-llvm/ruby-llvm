ad5597a741c8b589208417c51c67ce6ed4053c33
thoughtafter <thoughtafter@gmail.com>
2020-06-19 23:48:42 -0700

    upgrade to LLVM 10

--- lib/llvm/version.rb
+++ lib/llvm/version.rb
@@ -1,5 +1,5 @@
 module LLVM
-  LLVM_VERSION = "8".freeze
-  LLVM_REQUIRED_VERSION = "8.0".freeze
-  RUBY_LLVM_VERSION = "8.0.0".freeze
+  LLVM_VERSION = "10".freeze
+  LLVM_REQUIRED_VERSION = "10.0".freeze
+  RUBY_LLVM_VERSION = "10.0.0".freeze
 end
