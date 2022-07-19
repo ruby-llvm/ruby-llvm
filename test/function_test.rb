9ad9cc66f25838ac6b731773320de015e5eaafb2
thoughtafter <thoughtafter@gmail.com>
2022-07-18 22:32:13 -0700

    Upgrade to LLVM 14

--- test/function_test.rb
+++ test/function_test.rb
@@ -32,14 +32,14 @@ class FunctionTest < Minitest::Test
     end
   end
 
-  def helper_test_attribute(name)
+  def helper_test_attribute(name, attribute_id)
     with_function [], LLVM.Void do |fun|
       assert_equal 0, fun.attribute_count
       assert_equal [], fun.attributes
 
       fun.add_attribute(name)
       assert_equal 1, fun.attribute_count
-      assert_equal [34], fun.attributes
+      assert_equal [attribute_id], fun.attributes
 
       assert_equal false, fun.verify
 
@@ -52,11 +52,14 @@ class FunctionTest < Minitest::Test
   end
 
   def test_add_attribute_old_name
-    helper_test_attribute(:no_unwind_attribute)
+    helper_test_attribute(:no_unwind_attribute, 35)
   end
 
   def test_add_attribute_new_name
-    helper_test_attribute(:nounwind)
+    helper_test_attribute(:nounwind, 35)
+    helper_test_attribute(:readnone, 42)
+    helper_test_attribute(:readonly, 43)
+    helper_test_attribute(:willreturn, 64)
   end
 
 end
