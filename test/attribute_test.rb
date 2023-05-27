e868e6022e462851f9a421d65cf1c60b1c120b9c
thoughtafter <thoughtafter@gmail.com>
2023-05-27 14:08:27 -0700

    update test for LLVM 16

--- test/attribute_test.rb
+++ test/attribute_test.rb
@@ -25,7 +25,7 @@ class AttributeTestCase < Minitest::Test
   end
 
   def test_last_enum
-    assert_equal 85, LLVM::Attribute.last_enum
+    assert_equal 84, LLVM::Attribute.last_enum
   end
 
   def test_create_string
