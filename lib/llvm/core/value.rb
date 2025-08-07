22fb4f1c778f75f0ae40452d1589049a1865db28
thoughtafter <thoughtafter@gmail.com>
2025-08-07 13:38:17 -0700

    fix tests

--- lib/llvm/core/value.rb
+++ lib/llvm/core/value.rb
@@ -509,7 +509,8 @@ module LLVM
     # Multiplication.
     #: (ConstantInt) -> ConstantInt
     def *(rhs)
-      self.class.from_ptr(C.const_mul(self, rhs))
+      width = [type.width, rhs.type.width].max
+      LLVM::Type.integer(width).from_i(to_i * rhs.to_i, false)
     end
 
     alias_method :mul, :*
@@ -517,13 +518,15 @@ module LLVM
     # "No signed wrap" multiplication.
     #: (ConstantInt) -> ConstantInt
     def nsw_mul(rhs)
-      self.class.from_ptr(C.const_nsw_mul(self, rhs))
+      width = [type.width, rhs.type.width].max
+      LLVM::Type.integer(width).from_i(to_i * rhs.to_i, false)
     end
 
     # "No unsigned wrap" multiplication.
     #: (ConstantInt) -> ConstantInt
     def nuw_mul(rhs)
-      self.class.from_ptr(C.const_nuw_mul(self, rhs))
+      width = [type.width, rhs.type.width].max
+      LLVM::Type.integer(width).from_i(to_i * rhs.to_i, false)
     end
 
     # Unsigned division.
